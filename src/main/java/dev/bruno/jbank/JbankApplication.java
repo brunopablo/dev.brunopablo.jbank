804b09cd29c570b66807fd649f7d7db67dc165a1
Bruno <brn.pablox@gmail.com>
2026-01-20 19:53:00 -0300

    First

---
+++ src/main/java/dev/bruno/jbank/JbankApplication.java
@@ -0,0 +1,12 @@
+package dev.bruno.jbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JbankApplication.class, args);
+	}
+}
